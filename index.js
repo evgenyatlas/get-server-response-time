ec9a988e7acaa4335a31306ee275ce58af1756d7
geckyhit <gekcy@mail.ru>
2018-07-28 02:43:38 +0300

    Added restrictions on server waiting time

--- index.js
+++ index.js
@@ -2,28 +2,40 @@ const http = require('http')
 const https = require('https')
 const { URL } = require('url')
 
-module.exports = function getServerResponseTime(urlServer) {
+module.exports = function getServerResponseTime(urlServer, options = { timeout: 5000 }) {
     return new Promise((resolve, reject) => {
         try {
 
             const url = new URL(urlServer)
             let startTime = 0
+            let request = null
 
             switch (url.protocol) {
-
                 case 'http:':
+
                     startTime = Date.now()
-                    http.get(url, res => {
+
+                    request = http.get(url, _ => {
                         const endTime = Date.now() - startTime
                         resolve(endTime)
                     })
+                    request.setTimeout(options.timeout, () => {
+                        request.abort()
+                        throw new Error('Connection timed out. Check the server or increase the wait time in the options.timeout parameter')
+                    })
                     break
-
+                //FIX: do not repeat yourself
                 case 'https:':
+
                     startTime = Date.now()
-                    https.get(url, res => {
+
+                    request = https.get(url, _ => {
                         const endTime = Date.now() - startTime
                         resolve(endTime)
+                    })
+                    request.setTimeout(options.timeout, () => {
+                        request.abort()
+                        throw new Error('Connection timed out. Check the server or increase the wait time in the options.timeout parameter')
                     })
                     break
 
