08e6dd518c93c296c20f063ef407a7e5b5a9cc91
geckyhit <gekcy@mail.ru>
2018-08-14 23:47:16 +0300

    hiding unnecessary

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-const isBrowser = require('./util/isBrowser')
-
-module.exports = isBrowser() ? require('./lib/browser') : require('./lib/node')
+// does not work normally due to webpack. #find another way
+// const isBrowser = require('./util/isBrowser')
+// module.exports = isBrowser() ? require('./lib/browser') : require('./lib/node')
+module.exports = require('./lib/node')
